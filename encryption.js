0d914372b0eb047af0d49936f62f0cb70b158d24
杨畅 <yangchang23@m.fudan.edu.cn>
2026-07-14 14:25:53 +0800

    new

--- encryption.js
+++ encryption.js
@@ -1,14 +1,9 @@
 // encryption.js
 
-function showEncryptedContent(address) {
+function showEncryptedContent() {
     var password = prompt('Please Enter Password');
     if (password === '7777777') {
-        if (address==="index"){
         window.location.href = 'pdfs/CV_Chinese_version.pdf';
-       }
-       else if (address ==="subpages"){
-        window.location.href = '../pdfs/CV_Chinese_version.pdf';
-       }
     }
     else {
         alert('Failed');
